a0a109c90f8b89c3a5b070189e36cc28bdd39a56
Vimal-Ananth-B <vimalananthb@gmail.com>
2024-11-24 23:47:57 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'tamil', icon: <HomeIcon />, },
+  { name: 'Operation-D short film Smallboy Pictures', icon: <TheaterComedyIcon />, },
+  { name: 'Vj Siddhu Vlogs', icon: <LiveTvIcon /> },
+  { name: 'Plip plip', icon: <MusicNoteIcon />, },
+  { name: 'parithabangal', icon: <OndemandVideoIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'Video Songs', icon: <MusicNoteIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://ibb.co/pdB8F1t';
+export const demoChannelUrl = 'https://www.youtube.com/channel/UCnP5djI6D5HwJRUl30sgALw';
+export const demoVideoUrl = '/video/watch?v=wwEEwKrJ8VI';
+export const demoChannelTitle = 'Smallboy Pictures Operation-D short film';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
